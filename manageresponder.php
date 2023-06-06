ec225234ba661096fa9715ee0f96e9b0299f0c27
Reshan Gomis <reshangomis@gmail.com>
2023-06-07 00:33:06 +0530

    added responder deleting

--- manageresponder.php
+++ manageresponder.php
@@ -23,6 +23,7 @@ if (isset($_COOKIE["ManID"])) {
             <p>Support Page > Manager</p>
         </div>
         <div class="rightAlign">
+            <p class="button" onclick="goHome()">Home</p>
             <div class="profileImage" tooltip="NOT LOGGED IN" id="profilePic">
                 <img src="assets/profileicon.png" alt="profile icon" />
                 <button class="logout" onclick="logout()">Logout</button>
@@ -45,6 +46,7 @@ if (isset($_COOKIE["ManID"])) {
         if (isset($_GET["UpdateID"])) {
 
             $updateID = $_GET["UpdateID"];
+            setcookie("updateID",$updateID,time()+3600,"/");
 
             $resultResponderDetails = $conn->query("SELECT * FROM responder WHERE Res_ID = " . $updateID);
 
@@ -59,16 +61,72 @@ if (isset($_COOKIE["ManID"])) {
                         </form>
                     </fieldset>";
             }
+        } else if(isset($_GET["DeleteID"])){
 
-        } else if (isset($_GET["DeleteID"])) {
+            $deleteID = $_GET["DeleteID"];
+            $resultDelete = $conn->query("DELETE FROM responder WHERE Res_ID=".$deleteID);
 
+            if($resultDelete){
+                header("Location:http://localhost/Y1S2-Group-Project/manager.php");
+            }
+
+        }else {
+            echo "<fieldset>
+                        <legend>Add responder</legend>
+                        <form action=manageresponder.php method=post>
+                            <p>Name</p>  <input type=text name=nameAdd id=name><br>
+                            <p>Email</p>  <input type=text name=emailAdd id=email><br>
+                            <p>Password</p>  <input type=text name=pwdAdd id=pwd><br>
+                            <button class=button name=add>Update</button>
+                        </form>
+                    </fieldset>";
+        }
+
+
+        if (isset($_POST["update"])){
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $pwd = $_POST["pwd"];
+            $updateCookie = $_COOKIE["updateID"];
+
+            $resultUpdateRecord = $conn->query("UPDATE responder SET Res_username='$name' , Res_email='$email' , Res_password='$pwd' WHERE Res_ID=$updateCookie");
+
+            if ($resultUpdateRecord){
+                header("Location:http://localhost/Y1S2-Group-Project/manager.php");
+            }
+        }
+
+        if(isset($_POST["add"])){
+            $nameAdd = $_POST["nameAdd"];
+            $emailAdd = $_POST["emailAdd"];
+            $pwdAdd = $_POST["pwdAdd"];
+            $managerID = $_COOKIE["ManID"];
+            $IDAdd = 0;
+            $resultHighestID = $conn->query("SELECT * FROM responder");
+
+            while($row = $resultHighestID->fetch_assoc()){
+                if ($IDAdd < $row["Res_ID"]){
+                    $IDAdd = $row["Res_ID"];
+                }
+            }
+            $IDAdd++;
+
+            $resultInsert = $conn->query("INSERT INTO responder(Res_ID,Res_username,Res_email,Res_password,Man_ID) VALUES ($IDAdd,'$nameAdd','$emailAdd','$pwdAdd',$managerID)");
+            if ($resultInsert){
+                header("Location:http://localhost/Y1S2-Group-Project/manager.php");
+            }
         }
         ?>
 
 
     </div>
 
-
+<script>
+    function goHome() {
+        document.location = "http://localhost/Y1S2-Group-Project/manager.php";
+        document.cookie = "updateCookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    }
+</script>
 
 </body>
 
